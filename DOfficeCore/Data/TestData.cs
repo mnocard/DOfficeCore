9ee54166ca0efaaec7294d5a13d0fa8d1bb802a0
Хайрбек Мамбетов <mnocard@gmail.com>
2020-09-27 09:06:11 +0500

    Исправление доступности классов модели, создание тестовых данных, подключение тестовых данных к дереву и отображение его на представлении.

---
+++ DOfficeCore/Data/TestData.cs
@@ -0,0 +1,33 @@
+﻿using DOfficeCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace DOfficeCore.Data
+{
+    class TestData
+    {
+        public static List<string> blockList = Enumerable.Range(1, 20)
+            .Select(i => $"Line {i}")
+            .ToList();
+
+        public static List<Block> blocks = Enumerable.Range(1, 10)
+            .Select(i => new Block
+            {
+                Name = $"Block {i}",
+                Lines = new ObservableCollection<string>(blockList),
+            }).ToList();
+
+        public static List<Diagnosis> diag = Enumerable.Range(1, 10)
+            .Select(i => new Diagnosis
+            {
+                Code = $"Diagnosis {i}",
+                Blocks = new ObservableCollection<Block>(blocks),
+            }).ToList();
+
+        public static ObservableCollection<Diagnosis> diagnoses { get; set; } = new ObservableCollection<Diagnosis>(diag);
+        
+    }
+}
