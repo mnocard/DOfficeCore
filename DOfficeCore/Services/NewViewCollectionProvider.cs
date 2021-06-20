bb70ef1cc82bfe82e7cb738a2d2a9710a0952762
Хайрбек Мамбетов <mnocard@gmail.com>
2021-06-20 13:40:14 +0500

    Новый провайдер передачи данных

---
+++ DOfficeCore/Services/NewViewCollectionProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DOfficeCore.Models;
+using DOfficeCore.Services.Interfaces;
+
+namespace DOfficeCore.Services
+{
+    public class NewViewCollectionProvider : INewViewCollectionProvider
+    {
+        public Sector SearchDiagnosis(IEnumerable<Sector> SectorsList, string MultiBox)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Block> SearchBlocks(IEnumerable<Sector> SectorsList, string MultiBox)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> SearchLines(IEnumerable<Sector> SectorsList, string MultiBox)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
